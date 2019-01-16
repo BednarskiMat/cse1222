f72b93164a8a3219cc7698a9b98e02f26c611b89
Matt Bednarski <bednarskimat@Matts-MacBook-Pro.local>
2019-01-16 11:58:18 -0500

    add files

---
+++ lab/lab1/main.cpp
@@ -0,0 +1,20 @@
+// File: basics.cpp
+// Created by: Matthew Bednarski
+// Created on 1/15/19
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a;
+    int b, c;
+    char Character;
+    // solution here
+    cin >> a >> b >> c >> Character;
+
+    cout << a << b << c << Character;
+
+    cout << "Hello, World!" << endl;
+    return 0;
+}
